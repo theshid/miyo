89c643f132eae23050fbb9ac376c6492837f7dce
Rachid-Insa <rachidi@fidocredit.com>
2026-06-14 19:10:48 +0000

    Bump to versionCode 10 / versionName 1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "ani.saikou.v2"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.5.1"
+        versionCode = 10
+        versionName = "1.5.2"
 
         vectorDrawables {
             useSupportLibrary = true
